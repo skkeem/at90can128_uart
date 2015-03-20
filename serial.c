233e4c079bba1acadee9e9fb2ae748b8de049c4f
Skkeem <skkeem@gmail.com>
2015-03-21 04:05:57 +0900

    cleanup serial.c

--- serial.c
+++ serial.c
@@ -13,9 +13,6 @@ void initUart (unsigned int baud)
 	 */
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0); //0x18
 	UCSR0C = 3 << UCSZ0; //0x06
-
-//	UCSR0C = (0 << UMSEL0) | (0 << UPM0) | (0 << USBS0) | (3 << UCSZ0); //0x06
-//	UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | (1 << RXEN0) | (1 << TXEN0) | (0 << UCSZ02); //0x18
 }
 
 void putChar (char data)
